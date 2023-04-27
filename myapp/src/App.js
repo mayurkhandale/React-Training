8983a8f3700ee7eeec65e4a35a8d3f8e23e8d281
mayur <mjkhandale79@gmail.com>
2023-04-27 10:34:00 +0530

    main App

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App new </h1>
+      
+    </div>
+  );
+}
+
+export default App;
